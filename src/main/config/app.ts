a9ce48ef00cbe6a2e6e5e69f2472f87a14a7e0b9
leodeymison <leodeymisonbasquete@gmail.com>
2022-11-09 14:47:35 -0300

    feat: facivon icon

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import favicon from "serve-favicon";
 import { setupRoutes } from "./routes";
+import path from "path";
 
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(favicon(path.join('public', 'favicon.ico')))
 
 setupRoutes(app)
 
